b695689d7f6ba38360829b5cca0fd3963163f647
Lucas Gomez <lucas.gomez@tecnilogica.com>
2016-08-11 14:26:39 +0200

    Log in and Log out

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :posts
+
+  # Routes for authentication
+  post    'sessions'     => 'sessions#create'
+  delete  'sessions/:id' => 'sessions#destroy'
 end
